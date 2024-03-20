d2d017ee3adeb2ac74383def30fe695455387257
unknown <karimovsunatullo61@gmail.com>
2024-03-20 13:05:27 +0300

    searcg

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import  counterSlice  from "../reducers/Message/Message";
 import { videoReels } from "../reducers/reels/Reelse";
+import searchSlice from "../api/search/searchSlice";
 
 export const store = configureStore({
   reducer: {
     reels: videoReels,
-    message: counterSlice
+    message: counterSlice,
+    todo:searchSlice,
   },
+
 });
+
